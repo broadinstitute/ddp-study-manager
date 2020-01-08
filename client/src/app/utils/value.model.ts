9a209762311fd90ae6eff7c228804092e538d1ef
simone <simone@broadinstitute.org>
2020-01-08 15:40:42 -0500

    DDP-4318: - moved cover pdf checkboxes to displaySettings

--- client/src/app/utils/value.model.ts
+++ client/src/app/utils/value.model.ts
@@ -1,15 +1,17 @@
 export class Value {
 
   newAdded: boolean = false;
+  selected: boolean = true;
 
-  constructor( public value?: string, public type?: string, public type2?: string, public values?: Value[] ) {
+  constructor( public value?: string, public type?: string, public type2?: string, public name?: string, public values?: Value[] ) {
     this.value = value;
     this.type = type;
     this.type2 = type2;
+    this.name = name;
     this.values = values;
   }
 
   static parse( json ): Value {
-    return new Value( json.value, json.type, json.type2, json.values );
+    return new Value( json.value, json.type, json.type2, json.name, json.values );
   }
 }
