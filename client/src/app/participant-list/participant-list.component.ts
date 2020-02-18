66d4ccb9accb1685a7f1ec9abbb6605ffc5aa8ca
simone <simone@broadinstitute.org>
2020-02-18 15:40:08 -0500

    DDP-4374: - reformatting

--- client/src/app/participant-list/participant-list.component.ts
+++ client/src/app/participant-list/participant-list.component.ts
@@ -623,7 +623,7 @@ export class ParticipantListComponent implements OnInit {
         this.loadingParticipants = localStorage.getItem( ComponentService.MENU_SELECTED_REALM );
         this.dsmService.getParticipantData( localStorage.getItem( ComponentService.MENU_SELECTED_REALM ), participant.participant.ddpParticipantId, this.parent ).subscribe(
           data => {
-            if (data != null && data[0]!= null) {
+            if (data != null && data[ 0 ] != null) {
               let pt: Participant = Participant.parse( data[ 0 ] );
               if (pt == null || pt == undefined) {
                 this.errorMessage = "Participant  not found";
@@ -831,7 +831,7 @@ export class ParticipantListComponent implements OnInit {
   public shareFilter( savedFilter: ViewFilter, i ) {
     let value = savedFilter.shared ? "0" : "1";
     let patch1 = new PatchUtil( savedFilter.id, this.role.userMail(),
-      { name: "shared", value: value }, null, this.parent, null );
+      {name: "shared", value: value}, null, this.parent, null );
     let patch = patch1.getPatch();
     this.dsmService.patchParticipantRecord( JSON.stringify( patch ) ).subscribe( data => {
       let result = Result.parse( data );
@@ -845,7 +845,7 @@ export class ParticipantListComponent implements OnInit {
 
   public deleteView( savedFilter: ViewFilter ) {
     let patch1 = new PatchUtil( savedFilter.id, this.role.userMail(),
-      { name: "fDeleted", value: "1" }, null, this.parent, null );
+      {name: "fDeleted", value: "1"}, null, this.parent, null );
     let patch = patch1.getPatch();
     this.dsmService.patchParticipantRecord( JSON.stringify( patch ) ).subscribe( data => {
       let result = Result.parse( data );
