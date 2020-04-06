01f8c6e121a602a6022c5f3578f49927116000c5
simone <simone@broadinstitute.org>
2020-04-06 16:56:35 -0400

    DDP-3730: - fixing NA display - changing names to be unique

--- client/src/app/filter-column/models/column.model.ts
+++ client/src/app/filter-column/models/column.model.ts
@@ -24,8 +24,8 @@ export class ParticipantColumn {
   public static ONC_HISTORY_REVIEWED = new ParticipantColumn("Onc History Reviewed", "reviewedOncHistory", "o");
   public static PAPER_CR_SENT = new ParticipantColumn("Paper C/R Sent", "paperCRSent", "r");
   public static PAPER_CR_RECEIVED = new ParticipantColumn("Paper C/R Received", "paperCRReceived", "r");
-  public static PARTICIPANT_NOTES = new ParticipantColumn("Participant Note", "ptNotes", "r");
-  public static MINIMAL_RECORDS = new ParticipantColumn("Minimal Records", "minimalMR", "r");
+  public static PARTICIPANT_NOTES = new ParticipantColumn("Participant Notes", "ptNotes", "r");
+  public static MINIMAL_RECORDS = new ParticipantColumn("Incomplete/Minimal Medical Records", "minimalMR", "r");
   public static ABSTRACTION_READY = new ParticipantColumn("Ready for Abstraction", "abstractionReady", "r");
   public static ASSIGNEE_MR = new ParticipantColumn("MR Assignee", "assigneeMr", "p");
   public static ASSIGNEE_TISSUE = new ParticipantColumn("Tissue Assignee", "assigneeTissue", "p");
@@ -34,7 +34,7 @@ export class ParticipantColumn {
   //mr columns
   public static MR_TYPE = new ParticipantColumn("Type", "type", "inst");
   public static MR_INSTITUTION_NAME = new ParticipantColumn("Institution Name", "name", "m");
-  public static MR_INSTITUTION_CONTACT = new ParticipantColumn("Institution Contact", "contact", "m");
+  public static MR_INSTITUTION_CONTACT = new ParticipantColumn("Institution Contact Person", "contact", "m");
   public static MR_INSTITUTION_PHONE = new ParticipantColumn("Institution Phone", "phone", "m");
   public static MR_INSTITUTION_FAX = new ParticipantColumn("Institution Fax", "fax", "m");
   public static MR_FAX_SENT = new ParticipantColumn("Initial MR Request", "faxSent", "m");
@@ -42,63 +42,65 @@ export class ParticipantColumn {
   public static MR_FAX_SENT_3 = new ParticipantColumn("Initial MR Request 3", "faxSent3", "m");
   public static MR_RECEIVED = new ParticipantColumn("Initial MR Received", "mrReceived", "m");
   public static MR_DOCUMENT = new ParticipantColumn("MR Document", "mrDocument", "m");
-  public static MR_DOCUMENT_FILES = new ParticipantColumn("MR File Names", "mrDocumentFileNames", "m");
+  public static MR_DOCUMENT_FILES = new ParticipantColumn("MR Document File Names", "mrDocumentFileNames", "m");
   public static MR_PROBLEM = new ParticipantColumn("MR Problem", "mrProblem", "m");
   public static MR_PROBLEM_TEXT = new ParticipantColumn("MR Problem Text", "mrProblemText", "m");
-  public static MR_UNABLE_TO_OBTAIN = new ParticipantColumn("MR Unable to Obtain", "unableObtain", "m");
-  public static MR_DUPLICATE = new ParticipantColumn("MR Duplicate", "duplicate", "m");
-  public static MR_INTERNATIONAL = new ParticipantColumn("MR International", "international", "m");
-  public static MR_PAPER_CR = new ParticipantColumn("MR Paper C/R Required", "crRequired", "m");
+  public static MR_UNABLE_TO_OBTAIN = new ParticipantColumn("Unable to Obtain", "unableObtain", "m");
+  public static MR_DUPLICATE = new ParticipantColumn("Duplicate", "duplicate", "m");
+  public static MR_INTERNATIONAL = new ParticipantColumn("International", "international", "m");
+  public static MR_PAPER_CR = new ParticipantColumn("Paper C/R required", "crRequired", "m");
   public static PATHOLOGY_RESENT = new ParticipantColumn("Pathology Present", "pathologyPresent", "m");
-  public static MR_NOTES = new ParticipantColumn("MR Notes", "mrNotes", "m");
+  public static MR_NOTES = new ParticipantColumn("Notes", "mrNotes", "m");
   public static MR_REVIEW = new ParticipantColumn("MR Review", "reviewMedicalRecord", "m");
-  public static MR_FOLLOW_UP = new ParticipantColumn("MR Follow Ups", "followUps", "m");
+  public static MR_FOLLOW_UP = new ParticipantColumn("Follow-Up ", "followUps", "m");
+  public static MR_FOLLOW_REQUIRED = new ParticipantColumn("Follow-Up required", "followUpRequired", "m");
+  public static MR_FOLLOW_REQUIRED_TEXT = new ParticipantColumn("Follow-Up required Text", "followUpRequiredText", "m");
 
   //oncHistory columns
   public static ACCESSION_NUMBER = new ParticipantColumn("Accession Number", "accessionNumber", "oD");
-  public static DATE_PX = new ParticipantColumn("Date PX", "datePX", "oD");
-  public static TYPE_PX = new ParticipantColumn("Type PX", "typePX", "oD");
+  public static DATE_PX = new ParticipantColumn("Date of PX", "datePX", "oD");
+  public static TYPE_PX = new ParticipantColumn("Type of PX", "typePX", "oD");
   public static FACILITY = new ParticipantColumn("Facility", "facility", "oD");
   public static FACILITY_PHONE = new ParticipantColumn("Facility Phone", "fPhone", "oD");
   public static FACILITY_FAX = new ParticipantColumn("Facility Fax", "fFax", "oD");
   public static HISTOLOGY = new ParticipantColumn("Histology", "histology", "oD");
-  public static LOCATION_PX = new ParticipantColumn("Location PX", "locationPX", "oD");
-  public static ONC_HISTORY_NOTES = new ParticipantColumn("OncHistory Notes", "oncHisNotes", "oD");
+  public static LOCATION_PX = new ParticipantColumn("Location of PX", "locationPX", "oD");
+  public static ONC_HISTORY_NOTES = new ParticipantColumn("Notes", "oncHisNotes", "oD");
   public static ONC_HISTORY_REQUEST = new ParticipantColumn("Request Status", "request", "oD");
-  public static TISSUE_FAX = new ParticipantColumn("Tissue Fax Date", "tFaxSent", "oD");
-  public static TISSUE_FAX_2 = new ParticipantColumn("Tissue 1. Resend", "tFaxSent2", "oD");
-  public static TISSUE_FAX_3 = new ParticipantColumn("Tissue 2. Resend", "tFaxSent3", "oD");
+  public static TISSUE_FAX = new ParticipantColumn("Tissue Request Date", "tFaxSent", "oD");
+  public static TISSUE_FAX_2 = new ParticipantColumn("Tissue Request Date 2", "tFaxSent2", "oD");
+  public static TISSUE_FAX_3 = new ParticipantColumn("Tissue Request Date 3", "tFaxSent3", "oD");
   public static TISSUE_RECEIVED = new ParticipantColumn("Tissue Received", "tissueReceived", "oD");
   public static GENDER = new ParticipantColumn("Gender", "gender", "oD");
-  public static DESTRUCTION_POLICY = new ParticipantColumn("Destruction Policy", "destructionPolicy", "oD");
-  public static TISSUE_PROBLEM_OPTION = new ParticipantColumn("Tissue Problem", "tissueProblemOption", "oD");
+  public static DESTRUCTION_POLICY = new ParticipantColumn("Destruction Policy (years)", "destructionPolicy", "oD");
+  public static TISSUE_PROBLEM_OPTION = new ParticipantColumn("Problem with Tissue", "tissueProblemOption", "oD");
   public static UNABLE_OBTAIN_TISSUE = new ParticipantColumn("Unable To Obtain", "unableToObtain", "oD");
 
   //tissue column
   public static COUNT_RECEIVED = new ParticipantColumn("Count Received", "countReceived", "t");
-  public static TISSUE_NOTES = new ParticipantColumn("Tissue Notes", "tNotes", "t");
+  public static TISSUE_NOTES = new ParticipantColumn("Notes", "tNotes", "t");
   public static TISSUE_TYPE = new ParticipantColumn("Tissue Type", "tissueType", "t");
   public static TISSUE_SITE = new ParticipantColumn("Tissue Site", "tissueSite", "t");
   public static TUMOR_TYPE = new ParticipantColumn("Tumor Type", "tumorType", "t");
   public static H_E = new ParticipantColumn("H&E", "hE", "t");
   public static PATHOLOGY_REPORT = new ParticipantColumn("Pathology Report", "pathologyReport", "t");
   public static COLLABORATOR_SAMPLE_ID = new ParticipantColumn("Collaborator Sample ID", "collaboratorSampleId", "t");
-  public static BLOCK_SENT = new ParticipantColumn("Block Sent to SHL", "blockSent", "t");
-  public static SCROLL_RECEIVED = new ParticipantColumn("Scrolls Received", "scrollsReceived", "t");
+  public static BLOCK_SENT = new ParticipantColumn("Block to SHL", "blockSent", "t");
+  public static SCROLL_RECEIVED = new ParticipantColumn("Scrolls back from SHL", "scrollsReceived", "t");
   public static SK_ID = new ParticipantColumn("SK ID", "skId", "t");
   public static SM_ID = new ParticipantColumn("SM ID for H&E", "smId", "t");
-  public static SENT_GP = new ParticipantColumn("Sent GP", "sentGp", "t");
-  public static TISSUE_RETURNED = new ParticipantColumn("Tissue Returned", "tissueReturnDate", "t");
-  public static TISSUE_EXPECTED_RETURN = new ParticipantColumn("Tissue Expected Return", "expectedReturn", "t");
-  public static TISSUE_TRACKING_NUMBER = new ParticipantColumn("Tissue Tracking Number", "returnFedexId", "t");
+  public static SENT_GP = new ParticipantColumn("Date sent to GP", "sentGp", "t");
+  public static TISSUE_RETURNED = new ParticipantColumn("Return Date", "tissueReturnDate", "t");
+  public static TISSUE_EXPECTED_RETURN = new ParticipantColumn("Expected Return Date", "expectedReturn", "t");
+  public static TISSUE_TRACKING_NUMBER = new ParticipantColumn("Tracking Number", "returnFedexId", "t");
   public static TISSUE_SHL_NUMBER = new ParticipantColumn("SHL Work Number", "shlWorkNumber", "t");
   public static TISSUE_FIRST_SM_ID = new ParticipantColumn("First SM ID", "firstSmId", "t");
-  public static TISSUE_TUMOR_PERCENT = new ParticipantColumn("Tumor Percentage", "tumorPercentage", "t");
-  public static TISSUE_SEQUENCE = new ParticipantColumn("Sequence Results", "sequenceResults", "t");
-  public static SCROLLS_COUNT = new ParticipantColumn("Scrolls count", "scrollsCount", "t");
-  public static BLOCKS_COUNT = new ParticipantColumn("Blocks count", "blocksCount", "t");
-  public static USS_COUNT = new ParticipantColumn("USS count", "ussCount", "t");
-  public static H_E_COUNT = new ParticipantColumn("H&E count", "hECount", "t");
+  public static TISSUE_TUMOR_PERCENT = new ParticipantColumn("Tumor Percentage as reported by SHL", "tumorPercentage", "t");
+  public static TISSUE_SEQUENCE = new ParticipantColumn("Sequencing Results", "sequenceResults", "t");
+  public static SCROLLS_COUNT = new ParticipantColumn("Scroll(s)", "scrollsCount", "t");
+  public static BLOCKS_COUNT = new ParticipantColumn("Block(s)", "blocksCount", "t");
+  public static USS_COUNT = new ParticipantColumn("USS (unstained slides)", "ussCount", "t");
+  public static H_E_COUNT = new ParticipantColumn("H&E(s)", "hECount", "t");
 
 
   //abstraction column
