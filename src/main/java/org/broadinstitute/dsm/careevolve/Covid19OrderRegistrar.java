2196cc7b4b430ca3ab4f047a437cf0dff4c57c17
Andrew Zimmer <andrew@broadinstitute.org>
2020-08-03 13:56:26 -0400

    DDP-4815 running real queries against ES and translating DDP data into CareEvolve

--- src/main/java/org/broadinstitute/dsm/careevolve/Covid19OrderRegistrar.java
+++ src/main/java/org/broadinstitute/dsm/careevolve/Covid19OrderRegistrar.java
@@ -18,7 +18,6 @@ import org.broadinstitute.dsm.db.DDPInstance;
 import org.broadinstitute.dsm.exception.CareEvolveException;
 import org.broadinstitute.dsm.model.ParticipantWrapper;
 import org.broadinstitute.dsm.statics.DBConstants;
-import org.broadinstitute.dsm.util.ElasticSearchUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,34 +30,72 @@ public class Covid19OrderRegistrar {
 
     private final String endpoint;
 
+    private final String careEvolveAccount;
+
     /**
      * Create a new one that uses the given endpoint
      * for placing orders
      */
-    public Covid19OrderRegistrar(String endpoint) {
+    public Covid19OrderRegistrar(String endpoint, String careEvolveAccount) {
         this.endpoint = endpoint;
+        this.careEvolveAccount  = careEvolveAccount;
     }
 
-    public void orderTest(String hruid) {
+    public OrderResponse orderTest(Authentication auth,String participantHruid, String kitLabel,
+                                   String kitId) throws CareEvolveException {
 
         DDPInstance instance = DDPInstance.getDDPInstanceWithRole("testboston", DBConstants.HAS_KIT_REQUEST_ENDPOINTS);
 
         Map<String, String> queryConditions = new HashMap<>();
-        queryConditions.put("ES", " AND profile.hruid = '" + hruid +"'");
+        queryConditions.put("ES", " AND profile.hruid = '" + participantHruid +"'");
         List<ParticipantWrapper> participants = ParticipantWrapper.getFilteredList(instance, queryConditions);
-//participants.iterator().next().getData().get("profile")
-        //participants.iterator().next().getKits().iterator().next().getDsmKitId()
 
-        String userGuid = ((Map)participants.iterator().next().getData().get("profile")).get("guid").toString();
-        // how  to sort kit?
+
         if (participants.size() == 1) {
-            ParticipantWrapper participantWrapper = participants.iterator().next();
-            Map<String, Object> participantData = participantWrapper.getData();
+            ParticipantWrapper participant = participants.iterator().next();
+            Map data = participant.getData();
+            if (data != null) {
+                Map<String, String> address = (Map) data.get("address");
+                if (address != null) {
+                    Address careEvolveAddress = toCareEvolveAddress(address);
+                    logger.info("foo");
 
-            for (Map.Entry<String, Object> participantKeyValue : participantData.entrySet()) {
-                logger.info(participantKeyValue.getKey()  + "="  + participantKeyValue.getValue());
+                    Map<String, String> profile = (Map) participant.getData().get("profile");
+                    String patientId = profile.get("guid");
+
+                    String firstName = profile.get("firstName");
+                    String lastName = profile.get("lastName");
+                    // todo move provider to config file
+                    Provider provider = new Provider("Lisa", "Cosimi", "1154436111");
+
+                    List<AOE> aoes = AOE.forTestBoston(null, kitId);
+                    Patient testPatient = new Patient(patientId, firstName, lastName, "1901-01-01", "Other", "Other",
+                            "other", careEvolveAddress);
+
+                    Message message = new Message(new Order(careEvolveAccount, testPatient, kitLabel,provider, aoes), kitId);
+
+                    try {
+                        return orderTest(auth, message);
+                    } catch (IOException e) {
+                        throw new CareEvolveException("Could not order test for " + patientId, e);
+                    }
+                } else {
+                    throw new CareEvolveException("No address for " + participantHruid + ".  Cannot register order.");
+                }
+            } else {
+                throw new CareEvolveException("No participant data for " + participantHruid + ".  Cannot register order.");
             }
+        } else {
+            throw new CareEvolveException("No particiant data found for " + participantHruid + ".  Cannot register order.");
         }
+    }
+
+    private Address toCareEvolveAddress(Map<String, String> esAddress) {
+        return new Address(esAddress.get("street1"),
+                esAddress.get("street2"),
+                esAddress.get("city"),
+                esAddress.get("state"),
+                esAddress.get("zip"));
     }
 
     /**
@@ -66,7 +103,7 @@ public class Covid19OrderRegistrar {
      * Returns an {@link OrderResponse} when the order has been
      * placed successfully.  Otherwise, an exception is thrown.
      */
-    public OrderResponse orderTest(Authentication auth, Message message) throws IOException {
+    private OrderResponse orderTest(Authentication auth, Message message) throws IOException {
         logger.info("About to send {} as {} to {}", message.getName(), endpoint);
         AuthenticatedMessage careEvolveMessage = new AuthenticatedMessage(auth, message);
 
--- src/test/java/org/broadinstitute/dsm/careevolve/Covid19OrderRegistrarTest.java
+++ src/test/java/org/broadinstitute/dsm/careevolve/Covid19OrderRegistrarTest.java
@@ -1,9 +1,7 @@
 package org.broadinstitute.dsm.careevolve;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -11,13 +9,7 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.ddp.db.TransactionWrapper;
-import org.broadinstitute.dsm.TestHelper;
-import org.broadinstitute.dsm.db.DDPInstance;
-import org.broadinstitute.dsm.model.ParticipantWrapper;
 import org.broadinstitute.dsm.statics.ApplicationConfigConstants;
-import org.broadinstitute.dsm.statics.DBConstants;
-import org.broadinstitute.dsm.statics.RoutePath;
-import org.broadinstitute.dsm.util.ElasticSearchUtil;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -55,41 +47,10 @@ public class Covid19OrderRegistrarTest {
     }
 
     @Test
-    public void testSendCareEvoloveOrder() throws Exception {
+    public void testOrderForParticipant() throws Exception {
+        Covid19OrderRegistrar orderRegistrar = new Covid19OrderRegistrar(careEvolveOrderEndpoint, careEvolveAccount);
 
-        Covid19OrderRegistrar orderRegistrar = new Covid19OrderRegistrar(careEvolveOrderEndpoint);
-
-        // todo move provider to config file
-        Provider provider = new Provider("Lisa", "Cosimi", "1154436111");
-        String pepperUserGuid = "1234";
-        String pepperKitGuid =  "789";
-
-
-
-        List<AOE> aoes = AOE.forTestBoston(pepperUserGuid, pepperKitGuid);
-        Address address = new Address("75 Ames St.",null,"Cambridge","MA","02421");
-        Patient testPatient = new Patient("123","foo","bar","1901-01-01","Other","Other",
-                "other",address);
-
-        Message message = new Message(new Order(careEvolveAccount, testPatient, provider, aoes), "GBF2929101");
-
-        OrderResponse orderResponse = orderRegistrar.orderTest(auth, message);
-        Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
-
-        logger.info("Order returned " + gson.toJson(orderResponse));
-
-        Assert.assertNotNull(orderResponse);
-        Assert.assertNull(orderResponse.getError());
-        Assert.assertTrue(StringUtils.isNotBlank(orderResponse.getHandle()));
-
-
-    }
-
-    @Test
-    public void testOrdereForParticipant() throws Exception {
-        Covid19OrderRegistrar orderRegistrar = new Covid19OrderRegistrar(careEvolveOrderEndpoint);
-
-        orderRegistrar.orderTest("PKDG8J");
+        orderRegistrar.orderTest(auth,"PKDG8J","GBF1213","kit124");
 
     }
 }
