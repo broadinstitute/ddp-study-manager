65b3c044efc544e8361bb3df68fb1d550748ee5b
pegahtah <ptaheri@broadinstitute.org>
2020-08-31 01:49:46 -0400

    Adding a column to `ddp_kit` to store the results and to log them into it.

--- src/main/java/org/broadinstitute/dsm/jobs/PubSubLookUpJob.java
+++ src/main/java/org/broadinstitute/dsm/jobs/PubSubLookUpJob.java
@@ -6,6 +6,8 @@ import com.google.cloud.pubsub.v1.Subscriber;
 import com.google.gson.Gson;
 import com.google.pubsub.v1.ProjectSubscriptionName;
 import com.google.pubsub.v1.PubsubMessage;
+import org.apache.commons.lang3.StringUtils;
+import org.broadinstitute.ddp.db.SimpleResult;
 import org.broadinstitute.dsm.model.birch.TestBostonResult;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -13,12 +15,16 @@ import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import static org.broadinstitute.ddp.db.TransactionWrapper.inTransaction;
+
 public class PubSubLookUpJob implements Job {
     private static final Logger logger = LoggerFactory.getLogger(PubSubLookUpJob.class);
-    private static String SQL_INSERT_RESULTS = "INSERT INTO ddp_kit_request ";
+
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         String projectId = "hl7-dev-274911";
@@ -34,6 +40,7 @@ public class PubSubLookUpJob implements Job {
                         // Handle incoming message, then ack the received message.
                         System.out.println("Id: " + message.getMessageId());
                         System.out.println("Data: " + message.getData().toStringUtf8());
+                        writeResultsIntoDB(message);
                         consumer.ack();
                     };
 
@@ -59,7 +66,27 @@ public class PubSubLookUpJob implements Job {
     public static void writeResultsIntoDB(PubsubMessage message){
         String data = message.getData().toStringUtf8();
         TestBostonResult testBostonResult = new Gson().fromJson(data, TestBostonResult.class);
-        testBostonResult = new Gson().fromJson(message.getData().toString(), TestBostonResult.class);
+        String query = "UPDATE ddp_kit SET  test_result = ?, result_date= ?  WHERE dsm_kit_id <> 0 and  dsm_kit_id  in ( select  dsm_kit_id from (select * from ddp_kit) as something where kit_label= ?  )";
+        TestBostonResult finalTestBostonResult = testBostonResult;
+        SimpleResult results = inTransaction((conn) -> {
+            SimpleResult dbVals = new SimpleResult();
+            try (PreparedStatement stmt = conn.prepareStatement(query)) {
+                if (StringUtils.isNotBlank(finalTestBostonResult.result) && StringUtils.isNotBlank(finalTestBostonResult.date) && StringUtils.isNotBlank(finalTestBostonResult.sampleId)) {
+                    stmt.setString(1, finalTestBostonResult.result);
+                    stmt.setString(2, finalTestBostonResult.date);
+                    stmt.setString(3, finalTestBostonResult.sampleId);
+                    stmt.executeUpdate();
+                }
+            }
+            catch (SQLException ex) {
+                dbVals.resultException = ex;
+            }
+            return dbVals;
+        });
+
+        if (results.resultException != null) {
+            throw new RuntimeException("Couldn't update the test results for kit label" + testBostonResult.sampleId, results.resultException);
+        }
 
     }
 }
