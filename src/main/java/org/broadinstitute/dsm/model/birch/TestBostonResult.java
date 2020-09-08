0d72a11bc295f3139688e7bbcb0e56ab98a82f63
pegahtah <ptaheri@broadinstitute.org>
2020-09-08 16:39:14 -0400

    The result Object updated

--- src/main/java/org/broadinstitute/dsm/model/birch/TestBostonResult.java
+++ src/main/java/org/broadinstitute/dsm/model/birch/TestBostonResult.java
@@ -2,6 +2,14 @@ package org.broadinstitute.dsm.model.birch;
 
 public class TestBostonResult {
     public String date;
-    public String sampleId;
-    public String result;
+//    public String sampleId;
+//    public String result;
+
+    private String orderMessageId;
+    private String institutionId;
+    private String sampleId;
+    private String result;
+    private String reason;
+    private String timeCompleted;
+    
 }
