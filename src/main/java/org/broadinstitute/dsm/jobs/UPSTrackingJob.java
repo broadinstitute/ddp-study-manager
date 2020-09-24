c8742e3bae1d5f54ef603721adbdde8524a4dcac
pegahtah <ptaheri@broadinstitute.org>
2020-09-24 10:05:36 -0400

    * changed select command for kits based on their label to avoid junks

--- src/main/java/org/broadinstitute/dsm/jobs/UPSTrackingJob.java
+++ src/main/java/org/broadinstitute/dsm/jobs/UPSTrackingJob.java
@@ -31,7 +31,7 @@ public class UPSTrackingJob implements Job {
 
     private static final Logger logger = LoggerFactory.getLogger(UPSTrackingJob.class);
     private static final String SQL_SELECT_KITS = "SELECT * FROM ddp_kit kit LEFT JOIN ddp_kit_request req " +
-            "ON (kit.dsm_kit_request_id = req.dsm_kit_request_id) WHERE req.ddp_instance_id = ? order by kit.dsm_kit_request_id ASC";
+            "ON (kit.dsm_kit_request_id = req.dsm_kit_request_id) WHERE req.ddp_instance_id = ? and kit_label not like \"%_1\" order by kit.dsm_kit_request_id ASC";
 
     private static final String SQL_UPDATE_UPS_TRACKING_STATUS = "UPDATE ddp_kit SET ups_tracking_status = ?, ups_tracking_date = ? " +
             "WHERE dsm_kit_id <> 0 and dsm_kit_id in ( SELECT dsm_kit_id FROM ( SELECT * from ddp_kit) as something WHERE something.tracking_to_id = ? );";
@@ -298,9 +298,10 @@ public class UPSTrackingJob implements Job {
                         type = type.substring(0, type.indexOf(' '));
                     }
                     if (!"D".equals(type)) {
-                        if (!kit.getKitLabel().contains("_")) {
-                            returnTrackingIds.put(kit.getExternalOrderNumber(), kit);
+                        if (kit.getKitLabel().contains("_1") && kit.getKitLabel().indexOf("_1") == kit.getKitLabel().length()-2) {
+                            kit.setKitLabel(kit.getKitLabel().substring(0, kit.getKitLabel().length() - 2));
                         }
+                        returnTrackingIds.put(kit.getExternalOrderNumber(), kit);
                     }
                 }
             }
