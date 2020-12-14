7cfeda64a1a405d93ea3f726a03f4d30a6a8ca34
pegahtah <ptaheri@broadinstitute.org>
2020-12-14 15:34:08 -0500

    * fixed Kit_EVENT error

--- src/main/java/org/broadinstitute/dsm/util/KitEvent.java
+++ src/main/java/org/broadinstitute/dsm/util/KitEvent.java
@@ -4,18 +4,13 @@ import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.ddp.handlers.util.Event;
 
 public class KitEvent extends Event {
-    private String eventInfo;
-    private String eventType;
-    private long eventDate;
     private String kitReasonType;
     private String kitRequestId;
 
     public String NORMAL = "NORMAL";
 
     public KitEvent(String eventInfo, String eventType, long eventDate, String kitReasonType, String kitRequestId) {
-        this.eventInfo = eventInfo;
-        this.eventType = eventType;
-        this.eventDate = eventDate;
+        super(eventInfo, eventType, eventDate);
         if (StringUtils.isBlank(kitReasonType)) {
             kitReasonType = NORMAL;
         }
