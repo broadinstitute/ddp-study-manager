17b6b1ebaa7c250518eacc98e9a26b0bbbbca81c
Iraki Khaladze <ikhaladz@broadinstitute.org>
2020-12-24 16:28:19 +0400

    Add MockServerClient to test

--- src/test/java/org/broadinstitute/dsm/log/SlackAppenderTest.java
+++ src/test/java/org/broadinstitute/dsm/log/SlackAppenderTest.java
@@ -1,11 +1,18 @@
 package org.broadinstitute.dsm.log;
 
+import org.junit.Test;
+import org.mockserver.client.server.MockServerClient;
+
 import static org.junit.Assert.*;
 
 public class SlackAppenderTest {
 
+    private MockServerClient mockServerClient;
 
+    @Test
     public void testSuccessfulLoggingToSlack() {
+        mockServerClient = mockServerClient.reset();
+
 
     }
 
