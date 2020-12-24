7fa98f6eb3075d3173daedb287adcb1bd70d59ca
gmakhara <gmakhara@broadinstitute.org>
2020-12-24 17:12:13 +0400

    started using TestHelper class

--- src/test/java/org/broadinstitute/dsm/log/SlackAppenderTest.java
+++ src/test/java/org/broadinstitute/dsm/log/SlackAppenderTest.java
@@ -1,18 +1,29 @@
 package org.broadinstitute.dsm.log;
 
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+
+import org.broadinstitute.dsm.TestHelper;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockserver.client.server.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
 
 import static org.junit.Assert.*;
 
-public class SlackAppenderTest {
-
-    private MockServerClient mockServerClient;
+public class SlackAppenderTest extends TestHelper {
 
     @Test
     public void testSuccessfulLoggingToSlack() {
-        mockServerClient = mockServerClient.reset();
+        TestHelper.startMockServer();
 
+        if (mockDDP.isRunning()) {
+            mockDDP.when(request().withPath("/mock_slack_test"))
+                    .respond(response()
+                            .withStatusCode(200)
+                            .withBody("ok"));
+        }
 
     }
 
