292d07be133dcef7b3e267b1c14b6c524c6ff1ed
Iraki Khaladze <ikhaladz@broadinstitute.org>
2020-12-28 16:07:59 +0400

    Create SlackAppender class

---
+++ src/main/java/org/broadinstitute/dsm/log/SlackAppender.java
@@ -0,0 +1,9 @@
+package org.broadinstitute.dsm.log;
+
+import org.broadinstitute.ddp.loggers.ErrorNotificationAppender;
+
+public class SlackAppender extends ErrorNotificationAppender {
+    public SlackAppender() {
+    }
+
+}
