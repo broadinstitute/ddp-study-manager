ee06a9a1abfb502f0a5800ef1d49617e50efe400
simone <simone@broadinstitute.org>
2020-12-30 14:45:21 -0500

    changed confirmation call to multiple calls

--- src/main/java/org/broadinstitute/dsm/jobs/ExternalShipperJob.java
+++ src/main/java/org/broadinstitute/dsm/jobs/ExternalShipperJob.java
@@ -27,8 +27,20 @@ public class ExternalShipperJob implements Job {
                 ExternalShipper shipper = (ExternalShipper) Class.forName(DSMServer.getClassName(kitType.getExternalShipper())).newInstance();//GBFRequestUtil
                 shipper.updateOrderStatusForPendingKitRequests(kitType.getInstanceId());
                 Instant now = Instant.now();
-                Instant dynamicStartTime = now.minus(5, ChronoUnit.DAYS);
-                shipper.orderConfirmation(dynamicStartTime.toEpochMilli(), now.toEpochMilli());
+                for( int i = 5; i > 0; i--) {
+                    Instant dynamicStartTime = now.minus(i, ChronoUnit.DAYS);
+                    long end = 0L;
+                    if (i != 1) {
+                        Instant dynamicEndTime = now.minus(i - 1, ChronoUnit.DAYS);
+                        end = dynamicEndTime.toEpochMilli();
+                    }
+                    else {
+                        end = now.toEpochMilli();
+                    }
+                    shipper.orderConfirmation(dynamicStartTime.toEpochMilli(), end);
+                }
+//                Instant dynamicStartTime = now.minus(5, ChronoUnit.DAYS);
+//                shipper.orderConfirmation(dynamicStartTime.toEpochMilli(), now.toEpochMilli());
             }
             catch (Exception e) {
                 throw new RuntimeException("Failed to get status and/or confirmation ", e);
