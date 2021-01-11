93e0b282b48c2b4c1b66d5c9eb6adc1dc8b3f18b
gmakhara <gmakhara@broadinstitute.org>
2021-01-11 12:51:18 +0400

    DDP-5445 | started adding unit test for cloud function

---
+++ src/test/java/org/broadinstitute/dsm/functions/cloud/TestBostonUPSTrackingCloudFunctionTest.java
@@ -0,0 +1,36 @@
+package org.broadinstitute.dsm.functions.cloud;
+
+import static org.junit.Assert.*;
+
+import org.broadinstitute.dsm.jobs.TestBostonUPSTrackingJob;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestBostonUPSTrackingCloudFunctionTest {
+
+    private TestBostonUPSTrackingCloudFunction testBostonUPSTrackingCloudFunction;
+
+    private static final Logger logger = LoggerFactory.getLogger(TestBostonUPSTrackingJob.class);
+
+
+    @Before
+    public void setUp() {
+        testBostonUPSTrackingCloudFunction = new TestBostonUPSTrackingCloudFunction();
+    }
+
+    @Test
+    public void helloPubSub_shouldPrintName() {
+        TestBostonUPSTrackingCloudFunction.PubSubMessage pubSubMessage = new TestBostonUPSTrackingCloudFunction.PubSubMessage();
+
+        try {
+            testBostonUPSTrackingCloudFunction.accept(pubSubMessage, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
