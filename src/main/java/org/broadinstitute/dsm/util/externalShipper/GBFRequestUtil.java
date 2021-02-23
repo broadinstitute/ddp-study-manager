c06cdb44218c29001b0b6c604a58ce3e943c405b
simone <simone@broadinstitute.org>
2021-02-22 20:27:35 -0500

    removing secrets

--- src/main/java/org/broadinstitute/dsm/util/externalShipper/GBFRequestUtil.java
+++ src/main/java/org/broadinstitute/dsm/util/externalShipper/GBFRequestUtil.java
@@ -160,9 +160,9 @@ public class GBFRequestUtil implements ExternalShipper {
 
                 //                logger.info("orderXML: " + orderXml);
                 boolean test = DSMServer.isTest(getExternalShipperName());//true for dev in `not-secret.conf`
-                JSONObject payload = new JSONObject().put("orderXml", orderXml).put("test", false);
-                String sendRequest ="https://www.gbfmedical.com/oap/api/" + ORDER_ENDPOINT;
-                String apiKey = "ZDMxY2ZlMjctYTU0OS00Y2MxLWIyMmItNDYyNzI1MzJlOWJi";
+                JSONObject payload = new JSONObject().put("orderXml", orderXml).put("test", test);
+                String sendRequest = DSMServer.getBaseUrl(getExternalShipperName()) + ORDER_ENDPOINT;
+                String apiKey = DSMServer.getApiKey(getExternalShipperName());
                 Response gbfResponse = null;
                 int totalAttempts = 2 + additionalAttempts;
                 Exception ex = null;
