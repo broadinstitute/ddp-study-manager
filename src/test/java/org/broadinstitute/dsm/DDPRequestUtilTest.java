6c336dd4f0bf348c0f66560d8868335cd6d95635
SimoneMaiwald <simone@broadinstitute.org>
2021-04-01 13:44:59 -0400

    DDP-5610: RGP form (#140)
    * removing unused imports
    
    * DDP-5610: adding RGP form to liquidbase
    
    * DDP-5610: updating RGP form to liquidbase
    
    * DDP-5610: added a new ES test and fixed ES npe when pt is broken
    
    * DDP-5610: - removed unused imports
    - updated liquidbase
    
    * DDP-5610: - added workflow export for ES
    - updated liquidbase
    
    * DDP-5610: - updated workflow export for ES
    
    * DDP-5610: - fixing typos
    
    * DDP-5610: updated actions for RGP participation status
    
    * DDP-5610: - added default information to RGP form
    - added TAB and TAB_GROUPED information to tab
    
    * moved prod TestBostonUPSTrackingJob fix into branch
    
    * DDP-5610: - changed type
    - added options
    
    * DDP-5610: - added actions for SAMPLE replacement and AGE calc

--- src/test/java/org/broadinstitute/dsm/DDPRequestUtilTest.java
+++ src/test/java/org/broadinstitute/dsm/DDPRequestUtilTest.java
@@ -2,7 +2,6 @@ package org.broadinstitute.dsm;
 
 import org.broadinstitute.ddp.handlers.util.FollowUpSurvey;
 import org.broadinstitute.ddp.handlers.util.SimpleFollowUpSurvey;
-import org.broadinstitute.dsm.TestHelper;
 import org.broadinstitute.dsm.db.DDPInstance;
 import org.broadinstitute.dsm.util.DDPRequestUtil;
 import org.junit.AfterClass;
