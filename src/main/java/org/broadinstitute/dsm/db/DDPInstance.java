eb55085e4da0de07264eddef94259c31aca19eb0
simone <simone@broadinstitute.org>
2021-04-05 10:53:48 -0400

    changes from branch: DDP-5761_configurable-study-status

--- src/main/java/org/broadinstitute/dsm/db/DDPInstance.java
+++ src/main/java/org/broadinstitute/dsm/db/DDPInstance.java
@@ -407,7 +407,7 @@ public class DDPInstance {
                     while (rs.next()) {
                         DDPInstance ddpInstance = getDDPInstanceFormResultSet(rs);
                         List<Value> kitBehavior = Arrays.asList(new Gson().fromJson(rs.getString(DBConstants.KIT_BEHAVIOR_CHANGE), Value[].class));
-                        InstanceSettings instanceSettings = new InstanceSettings(null, kitBehavior, null, null, false);
+                        InstanceSettings instanceSettings = new InstanceSettings(null, kitBehavior, null, null, null, false);
                         ddpInstance.setInstanceSettings(instanceSettings);
                         ddpInstances.add(ddpInstance);
                     }
