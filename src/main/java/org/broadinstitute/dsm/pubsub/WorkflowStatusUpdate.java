8b157982aaa90fa696ef87565bc8783fd748ffbb
Irakli Khaladze <75846629+Irakli-Khaladze@users.noreply.github.com>
2021-06-21 16:37:07 +0400

    Add new participantData in ddp_participant_data and export workflows … (#203)
    * Add new participantData in ddp_participant_data and export workflows only if necessary
    
    * Fix some issues related to logic and code
    
    * Just to check add instance to column to get fieldsetting
    
    * pas instnce id as int

--- src/main/java/org/broadinstitute/dsm/pubsub/WorkflowStatusUpdate.java
+++ src/main/java/org/broadinstitute/dsm/pubsub/WorkflowStatusUpdate.java
@@ -3,12 +3,20 @@ package org.broadinstitute.dsm.pubsub;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.broadinstitute.dsm.db.DDPInstance;
+import org.broadinstitute.dsm.db.dao.fieldsettings.FieldSettingsDao;
 import org.broadinstitute.dsm.db.dao.participant.data.ParticipantDataDao;
+import org.broadinstitute.dsm.db.dto.fieldsettings.FieldSettingsDto;
 import org.broadinstitute.dsm.db.dto.participant.data.ParticipantDataDto;
+import org.broadinstitute.dsm.export.ExportToES;
+import org.broadinstitute.dsm.model.Value;
+import org.broadinstitute.dsm.statics.ESObjectConstants;
 import org.broadinstitute.dsm.util.ElasticSearchUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class WorkflowStatusUpdate {
     public static final String STUDY_GUID = "studyGuid";
@@ -16,10 +24,11 @@ public class WorkflowStatusUpdate {
     public static final String MEMBER_TYPE = "MEMBER_TYPE";
     public static final String SELF = "SELF";
     public static final String DSS = "DSS";
-    public static final String RGP = "RGP";
     private static final Gson gson = new Gson();
 
+    private static final Logger logger = LoggerFactory.getLogger(ExportToES.class);
     private static final ParticipantDataDao participantDataDao = new ParticipantDataDao();
+    private static final FieldSettingsDao fieldSettingsDao = new FieldSettingsDao();
 
     public static void updateCustomWorkflow(Map<String, String> attributesMap, String data) {
         WorkflowPayload workflowPayload = gson.fromJson(data, WorkflowPayload.class);
@@ -31,12 +40,52 @@ public class WorkflowStatusUpdate {
         DDPInstance instance = DDPInstance.getDDPInstanceByGuid(studyGuid);
 
         List<ParticipantDataDto> participantDatas = participantDataDao.getParticipantDataByParticipantId(ddpParticipantId);
+        Optional<FieldSettingsDto> fieldSetting = fieldSettingsDao.getFieldSettingByColumnNameAndInstanceId(Integer.parseInt(instance.getDdpInstanceId()), workflow);
+        if (fieldSetting.isEmpty()) {
+            logger.warn("Wrong workflow name");
+        } else {
+            FieldSettingsDto setting = fieldSetting.get();
+            boolean isOldParticipant = participantDatas.stream()
+                    .anyMatch(participantDataDto -> participantDataDto.getFieldTypeId().equals(setting.getFieldType())
+                            || !participantDataDto.getFieldTypeId().contains("GROUP"));
+            if (isOldParticipant) {
+                participantDatas.forEach(participantDataDto -> {
+                    updateProbandStatusInDB(workflow, status, participantDataDto, studyGuid);
+                });
+            } else {
+                addNewParticipantDataWithStatus(workflow, status, ddpParticipantId, setting);
+            }
+            exportToESifNecessary(workflow, status, ddpParticipantId, instance, setting);
+        }
 
-        participantDatas.forEach(participantDataDto -> {
-            updateProbandStatusInDB(workflow, status, participantDataDto, studyGuid);
-        });
+    }
 
-        ElasticSearchUtil.writeWorkflow(instance, ddpParticipantId, workflow, status);
+    public static void exportToESifNecessary(String workflow, String status, String ddpParticipantId, DDPInstance instance, FieldSettingsDto setting) {
+        String actions = setting.getActions();
+        if (actions != null) {
+            Value[] actionsArray =  gson.fromJson(actions, Value[].class);
+            for (Value action : actionsArray) {
+                if (ESObjectConstants.ELASTIC_EXPORT_WORKFLOWS.equals(action.getType())) {
+                    ElasticSearchUtil.writeWorkflow(instance, ddpParticipantId, workflow, status);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static int addNewParticipantDataWithStatus(String workflow, String status, String ddpParticipantId, FieldSettingsDto setting) {
+        JsonObject dataJsonObject = new JsonObject();
+        dataJsonObject.addProperty(workflow, status);
+        int participantDataId;
+        participantDataId = participantDataDao.create(new ParticipantDataDto(
+                ddpParticipantId,
+                setting.getDdpInstanceId(),
+                setting.getFieldType(),
+                dataJsonObject.toString(),
+                System.currentTimeMillis(),
+                WorkflowStatusUpdate.DSS)
+        );
+        return participantDataId;
     }
 
     public static void updateProbandStatusInDB(String workflow, String status, ParticipantDataDto participantDataDto, String studyGuid) {
@@ -45,7 +94,7 @@ public class WorkflowStatusUpdate {
             return;
         }
         JsonObject dataJsonObject = gson.fromJson(oldData, JsonObject.class);
-        if ((!RGP.equals(studyGuid) || isProband(dataJsonObject)) && dataJsonObject.get(workflow) != null) {
+        if ((participantDataDto.getFieldTypeId().contains("GROUP") || isProband(dataJsonObject))) {
             dataJsonObject.addProperty(workflow, status);
             participantDataDao.updateParticipantDataColumn(
                     new ParticipantDataDto(participantDataDto.getParticipantDataId(),
