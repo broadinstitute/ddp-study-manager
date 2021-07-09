37c9d735cb5ff5f2d1700da3d97baf2831429066
Iraki Khaladze <ikhaladz@broadinstitute.org>
2021-07-09 21:51:36 +0400

    DDP-6541 | fix test duplication which happened during merging

--- src/test/java/org/broadinstitute/dsm/FilterTest.java
+++ src/test/java/org/broadinstitute/dsm/FilterTest.java
@@ -140,44 +140,6 @@ public class FilterTest {
     }
 
     @Test
-    public void testAddOneParticipantDataFilterNotEmpty() {
-        Map<String, String> queryConditions = new HashMap<>();
-        Filter filter = new Filter(false, true, false, true, "DATE", "participantData",
-                new NameValue("PARTICIPANT_DEATH_DATE", "2040-10-35"), null, null, null);
-        List<ParticipantDataDto> allParticipantData = participantDataDao
-                .getParticipantDataByInstanceid(Integer.parseInt(String.valueOf(ddpInstanceDto.getDdpInstanceId())));
-        FilterRoute.addParticipantDataFilters(queryConditions, filter, filter.getFilter1().getName(), allParticipantData);
-        Assert.assertEquals("null AND (profile.legacyAltPid = " + participantId
-                + " OR profile.guid = " + participantId + ")", queryConditions.get(ElasticSearchUtil.ES));
-    }
-
-    @Test
-    public void testAddOneParticipantDataWithRange() {
-        Map<String, String> queryConditions = new HashMap<>();
-        Filter filter = new Filter(false, true, false, false, "DATE", "participantData",
-                new NameValue("PARTICIPANT_DEATH_DATE", "2040-10-29"),
-                new NameValue("PARTICIPANT_DEATH_DATE", "2040-10-31"), null, null);
-        List<ParticipantDataDto> allParticipantData = participantDataDao
-                .getParticipantDataByInstanceid(Integer.parseInt(String.valueOf(ddpInstanceDto.getDdpInstanceId())));
-        FilterRoute.addParticipantDataFilters(queryConditions, filter, filter.getFilter1().getName(), allParticipantData);
-        Assert.assertEquals("null AND (profile.legacyAltPid = " + participantId
-                + " OR profile.guid = " + participantId + ")", queryConditions.get(ElasticSearchUtil.ES));
-    }
-
-    @Test
-    public void testAddOneParticipantDataWithRangeFalse() {
-        Map<String, String> queryConditions = new HashMap<>();
-        Filter filter = new Filter(false, true, false, false, "DATE", "participantData",
-                new NameValue("PARTICIPANT_DEATH_DATE", "2020-10-29"),
-                new NameValue("PARTICIPANT_DEATH_DATE", "2020-10-31"), null, null);
-        List<ParticipantDataDto> allParticipantData = participantDataDao
-                .getParticipantDataByInstanceid(Integer.parseInt(String.valueOf(ddpInstanceDto.getDdpInstanceId())));
-        FilterRoute.addParticipantDataFilters(queryConditions, filter, filter.getFilter1().getName(), allParticipantData);
-        Assert.assertNotEquals("null AND (profile.legacyAltPid = " + participantId
-                + " OR profile.guid = " + participantId + ")", queryConditions.get(ElasticSearchUtil.ES));
-    }
-
-    @Test
     public void testAddSeveralParticipantDataFilter() {
         Map<String, String> queryConditions = new HashMap<>();
         Filter filterA = new Filter(false, true, false, false, "DATE", "participantData",
