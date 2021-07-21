1ce4f68430768ec22390b29b1fcbf232cf5d8e37
gmakharat <75846598+gmakharat@users.noreply.github.com>
2021-07-21 18:45:31 +0400

    DDP-6591 | family member workflow only for shared email (#250)
    * DDP-6591 | add family member package by feature, refactor code
    
    * DDP-6591 | add add family member class for rgp, refactor code
    
    * DDP-6591 | change AddFamilyMember class from abstract to normal to provide default implementation of AddFamilyMember
    
    * DDP-6591 | remove not used test, moved UserDto to dto directory
    
    * DDP-6591 | change participantId to ddpParticipantId
    
    * DDP-6591 | add method to control workflow by email, if family member does not share applicant email anymore remove its workflow from es, if he/she started sharing email export his/her already existing data to ES workflow
    
    * DDP-6591 | fix merge conflicts

--- src/main/java/org/broadinstitute/dsm/model/settings/field/FieldSettings.java
+++ src/main/java/org/broadinstitute/dsm/model/settings/field/FieldSettings.java
@@ -3,12 +3,14 @@ package org.broadinstitute.dsm.model.settings.field;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import com.google.gson.Gson;
 import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.dsm.db.dto.settings.FieldSettingsDto;
+import org.broadinstitute.dsm.db.dao.settings.FieldSettingsDao;
 import org.broadinstitute.dsm.model.Value;
 import org.broadinstitute.dsm.statics.ESObjectConstants;
 import org.slf4j.Logger;
@@ -20,6 +22,11 @@ public class FieldSettings {
 
     public static final String KEY_DEFAULT = "default";
     public static final String KEY_VALUE = "value";
+    private final FieldSettingsDao fieldSettingsDao;
+
+    public FieldSettings() {
+        this.fieldSettingsDao = FieldSettingsDao.of();
+    }
 
     public Map<String, String> getColumnsWithDefaultOptions(@NonNull List<FieldSettingsDto> fieldSettingsDtos) {
         Map<String, String> defaultOptions = new HashMap<>();
@@ -77,4 +84,9 @@ public class FieldSettings {
                 .anyMatch(act -> ESObjectConstants.ELASTIC_EXPORT_WORKFLOWS.equals(act.getType()));
     }
 
+    public boolean isColumnExportable(int instanceId, String columnName) {
+        Optional<FieldSettingsDto> maybeFieldSetting =
+                fieldSettingsDao.getFieldSettingByColumnNameAndInstanceId(instanceId, columnName);
+        return isElasticExportWorkflowType(maybeFieldSetting.map(FieldSettingsDto::getActions).orElse(""));
+    }
 }
