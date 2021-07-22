ba9ec96700608e1f0e0de9733f5d733b8a4a747b
Andrew Zimmer <andrew@broadinstitute.org>
2021-07-22 09:01:40 -0400

    Removing kit ordering CF now that tbos is ramping down

--- build-and-deploy-tbos-cloud-functions.sh
+++ build-and-deploy-tbos-cloud-functions.sh
@@ -20,22 +20,6 @@ gcloud --project=${PROJECT_ID} functions deploy \
 
 
 
-
-echo "Deploying testboston kit dispatcher to ${PROJECT_ID}"
-gcloud --project=${PROJECT_ID} functions deploy \
-    tbos-kit-dispatcher \
-    --entry-point=org.broadinstitute.dsm.cf.TestBostonKitOrderer \
-    --runtime=java11 \
-    --trigger-topic=tbos-kit-dispatcher \
-    --timeout=540 \
-    --memory=1Gib \
-    --source=target/deployment \
-    --set-env-vars="PROJECT_ID=${PROJECT_ID},SECRET_ID=study-manager-config,DDP_INSTANCE=testboston,EMAIL_TO=testboston-notifications@broadinstitute.org,EMAIL_FROM=notifications-noreply@datadonationplatform.org,SHIPPING_RATE=3rd Day Air Residential" \
-    --vpc-connector=projects/${PROJECT_ID}/locations/us-central1/connectors/appengine-default-connect
-
-
-
-
 echo "Deploying testboston kit filter report to ${PROJECT_ID}"
 gcloud --project=${PROJECT_ID} functions deploy \
     tbos-kit-filter-report \
