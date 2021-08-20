f32773b35eea2e2bde8585a7d8ae3e0d6ae84412
simone <simone@broadinstitute.org>
2021-08-20 14:14:58 -0400

    adding hotfix for studySpecificStatuses (https://github.com/broadinstitute/ddp-study-manager/pull/280)

--- src/main/java/org/broadinstitute/dsm/db/dao/settings/InstanceSettingsDao.java
+++ src/main/java/org/broadinstitute/dsm/db/dao/settings/InstanceSettingsDao.java
@@ -26,7 +26,7 @@ public class InstanceSettingsDao implements Dao<InstanceSettingsDto> {
             "FROM instance_settings " +
             "WHERE ddp_instance_id = (SELECT ddp_instance_id FROM ddp_instance WHERE study_guid = ?)";
 
-    private static final String SQL_GET_BY_STUDY_GUID = "SELECT " +
+    private static final String SQL_GET_BY_INSTANCE_NAME = "SELECT " +
             "instance_settings_id, " +
             "ddp_instance_id, " +
             "mr_cover_pdf, " +
@@ -39,7 +39,7 @@ public class InstanceSettingsDao implements Dao<InstanceSettingsDto> {
             "has_invitations, " +
             "GBF_SHIPPED_DSS_DELIVERED " +
             "FROM instance_settings " +
-            "WHERE ddp_instance_id = (SELECT ddp_instance_id FROM ddp_instance WHERE study_guid = ?)";
+            "WHERE ddp_instance_id = (SELECT ddp_instance_id FROM ddp_instance WHERE instance_name = ?)";
 
     public static final String HIDE_SAMPLES_TAB = "hide_samples_tab";
     public static final String INSTANCE_SETTINGS_ID = "instance_settings_id";
@@ -92,30 +92,30 @@ public class InstanceSettingsDao implements Dao<InstanceSettingsDto> {
         return Optional.ofNullable((Boolean) results.resultValue);
     }
 
-    public Optional<InstanceSettingsDto> getByStudyGuid(String studyGuid) {
-        SimpleResult results = inTransaction((conn) -> getInstanceSettingsByStudyGuid(studyGuid, conn));
+    public Optional<InstanceSettingsDto> getByInstanceName(String instanceName) {
+        SimpleResult results = inTransaction((conn) -> getInstanceSettingsByInstanceName(instanceName, conn));
         if (results.resultException != null) {
             throw new RuntimeException("Error getting instance settings for study guid: "
-                    + studyGuid, results.resultException);
+                    + instanceName, results.resultException);
         }
         return Optional.ofNullable((InstanceSettingsDto) results.resultValue);
     }
 
     //used ONLY for google cloud function
-    public Optional<InstanceSettingsDto> getByStudyGuid(Connection conn, String studyGuid) {
-        SimpleResult results = getInstanceSettingsByStudyGuid(studyGuid, conn);
+    public Optional<InstanceSettingsDto> getByInstanceName(Connection conn, String instanceName) {
+        SimpleResult results = getInstanceSettingsByInstanceName(instanceName, conn);
         if (results.resultException != null) {
             throw new RuntimeException("Error getting instance settings for study guid: "
-                    + studyGuid, results.resultException);
+                    + instanceName, results.resultException);
         }
         return Optional.ofNullable((InstanceSettingsDto) results.resultValue);
     }
 
 
-    private SimpleResult getInstanceSettingsByStudyGuid(String studyGuid, Connection conn) {
+    private SimpleResult getInstanceSettingsByInstanceName(String instanceName, Connection conn) {
         SimpleResult execResult = new SimpleResult();
-        try (PreparedStatement stmt = conn.prepareStatement(SQL_GET_BY_STUDY_GUID)) {
-            stmt.setString(1, studyGuid);
+        try (PreparedStatement stmt = conn.prepareStatement(SQL_GET_BY_INSTANCE_NAME)) {
+            stmt.setString(1, instanceName);
             try(ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
                     execResult.resultValue = new InstanceSettingsDto.Builder()
