50afbeb30792360e2ad52b9fd9382a863182c923
gmakhara <gmakhara@broadinstitute.org>
2021-10-19 18:06:36 +0400

    DDP-7047 | fix the test

--- src/test/java/org/broadinstitute/dsm/model/elastic/export/SourceGeneratorTest.java
+++ src/test/java/org/broadinstitute/dsm/model/elastic/export/SourceGeneratorTest.java
@@ -16,7 +16,7 @@ public class SourceGeneratorTest {
         NameValue nameValue = new NameValue("medical_record_column", "value");
         Map<String, Object> objectMap = generator.generate(nameValue);
         Assert.assertEquals(objectMap.keySet().stream().findFirst().get(), SourceGenerator.DSM_OBJECT);
-        String value = (String)((Map)((Map)objectMap.get(SourceGenerator.DSM_OBJECT)).get("m")).get("medical_record_column");
+        String value = (String)((Map)((Map)objectMap.get(SourceGenerator.DSM_OBJECT)).get("medicalRecords")).get("medical_record_column");
         Assert.assertEquals("value", value);
     }
 
@@ -26,6 +26,15 @@ public class SourceGeneratorTest {
         protected DBElement getDBElement(NameValue nameValue) {
             return TestPatchUtil.getColumnNameMap().get(nameValue.getName());
         }
+    }
+
+    private static class TestPatchUtil extends PatchUtil {
+
+        public static Map<String, DBElement> getColumnNameMap() {
+            DBElement dbElement = new DBElement("ddp_medical_record", "m", "pr", "medical_record_column");
+            return Map.of("medical_record_column", dbElement);
+        }
+
     }
 }
 
