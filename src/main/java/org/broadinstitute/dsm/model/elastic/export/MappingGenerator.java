b5afc8532996246bcb52fdbe7bb527bb5a0af26b
gmakhara <gmakhara@broadinstitute.org>
2021-10-20 16:23:35 +0400

    DDP-7047 | start implementing logic for parses for elastic mapper

--- src/main/java/org/broadinstitute/dsm/model/elastic/export/MappingGenerator.java
+++ src/main/java/org/broadinstitute/dsm/model/elastic/export/MappingGenerator.java
@@ -1,8 +1,13 @@
 package org.broadinstitute.dsm.model.elastic.export;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Predicate;
 
 import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.dsm.model.NameValue;
@@ -15,26 +20,44 @@ public class MappingGenerator extends BaseGenerator {
         initializeNecessaryFields(Objects.requireNonNull(nameValue));
         String property = TABLE_ALIAS_MAPPINGS.get(dbElement.getTableAlias());
         String value = (String) nameValue.getValue();
-
-        StringUtils.isNumeric("asd");
-        Boolean.valueOf("true");
-        // Date, time, datetime
-        
+        String type = "text";
 
 
+        if (StringUtils.isNumeric(value)) {
+            type = "integer";
+        } else if (isBoolean(value)) {
+            type = "boolean";
+        } else if (isDateTime(value)) {
+            type = "date";
+        }
+
+        //properties.dsm.properties.medicalRecords -> {"type": "boolean"}
         Map<String, Object> jsonMap = new HashMap<>();
         Map<String, Object> message = new HashMap<>();
-        message.put("type", "");
+        message.put("type", type);
         Map<String, Object> properties = new HashMap<>();
         properties.put("message", message);
         jsonMap.put("properties", properties);
-        request.source(jsonMap);
-
+        // Date, time, datetime
 
 
         return null;
     }
 
+    private boolean isDateTime(String value) {
+        try {
+            LocalDate.parse(value);
+            LocalTime.parse(value);
+            LocalDateTime.parse(value);
+        } catch (DateTimeParseException dtpe) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isBoolean(String value) {
+        return value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false");
+    }
 
 
 }
