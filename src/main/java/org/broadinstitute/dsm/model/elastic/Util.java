b71bcf1b0f1ecf4673c5348b308cd7df772f161d
Nika <nghurtch@broadinstitute.org>
2021-10-27 18:00:09 +0400

    DDP-7047 | add export() to OncHistoryDetailPatch.java

--- src/main/java/org/broadinstitute/dsm/model/elastic/Util.java
+++ src/main/java/org/broadinstitute/dsm/model/elastic/Util.java
@@ -16,8 +16,7 @@ public class Util {
             "t", new BaseGenerator.PropertyInfo(ESObjectConstants.TISSUE_RECORDS, true),
             "oD", new BaseGenerator.PropertyInfo(ESObjectConstants.ONC_HISTORY_DETAIL_RECORDS, true),
             "r", new BaseGenerator.PropertyInfo("participant", false),
-            "p", new BaseGenerator.PropertyInfo("participant", false),
-            "d", new BaseGenerator.PropertyInfo("participant", false)
+            "p", new BaseGenerator.PropertyInfo("participant", false)
     );
 
     public static String getQueryTypeFromId(String id) {
