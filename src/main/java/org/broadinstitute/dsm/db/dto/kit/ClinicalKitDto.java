498fa2fee035d942cca4763dd39cfa6c26af56b8
SimoneMaiwald <simone@broadinstitute.org>
2021-11-01 12:16:40 -0400

    DDP-7010: changes for mercury dummy endpoint (#313)
    * added sample type to clinical kit
    
    * added frst/last name
    
    * added frst/last name
    
    * added gender from onc history
    removed sample type from liquibase and hard coded it
    
    * DDP-7010: only select active kits
    
    * DDP-7010: - updated return fields for mercury
    - getting gender from activity if info was not already added on the tissue page
    
    Co-authored-by: pegahtah <ptaheri@broadinstitute.org>

--- src/main/java/org/broadinstitute/dsm/db/dto/kit/ClinicalKitDto.java
+++ src/main/java/org/broadinstitute/dsm/db/dto/kit/ClinicalKitDto.java
@@ -21,11 +21,58 @@ public class ClinicalKitDto {
     @SerializedName ("vessel_type")
     String vesselType;
 
-    @SerializedName ("mail_to_name")
-    String mailToName;
+    @SerializedName ("first_name")
+    String firstName;
+
+    @SerializedName ("last_name")
+    String lastName;
 
     @SerializedName ("date_of_birth")
     String dateOfBirth;
+
+    @SerializedName("sample_type")
+    String sampleType;
+
+    @SerializedName("gender")
+    String gender;
+
+    @SerializedName("accession_number")
+    String accessionNumber;
+
+    @SerializedName("collection_date")
+    String collectionDate;
+
+    @SerializedName("kit_label")
+    String mfBarcode;
+
     public ClinicalKitDto(){}
+
+    public void setSampleType(String kitType){
+        switch (kitType.toLowerCase()){
+            case "saliva":
+                this.sampleType = "Normal";
+                break;
+            case "blood":
+                this.sampleType = "N/A";
+                break;
+            default: //tissue
+                this.sampleType = "Tumor";
+                break;
+        }
+    }
+
+    public void setGender(String genderString){
+        switch (genderString.toLowerCase()){
+            case "male":
+                this.gender = "M";
+                break;
+            case "female":
+                this.gender = "F";
+                break;
+            default: //intersex or prefer_not_answer
+                this.gender = "U";
+                break;
+        }
+    }
 
 }
