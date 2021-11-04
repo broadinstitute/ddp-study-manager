d5884754d31363b0fef9a802f59d0dfb824beed6
Nika <nghurtch@broadinstitute.org>
2021-11-04 16:55:28 +0400

    DDP-7117 | finish KitRequestShippingMigrate

--- src/main/java/org/broadinstitute/dsm/model/elastic/migrationscript/KitRequestShippingMigrate.java
+++ src/main/java/org/broadinstitute/dsm/model/elastic/migrationscript/KitRequestShippingMigrate.java
@@ -9,49 +9,70 @@ import java.util.Map;
 public class KitRequestShippingMigrate {
 
     private static final Map<String, Object> kitRequestShippingMigrate1 = Map.of (
+            "kitTypeName", TEXT_KEYWORD_MAPPING,
+            "instanceName", TEXT_KEYWORD_MAPPING,
+            "bspCollaboratorParticipantId", TEXT_KEYWORD_MAPPING,
+            "bspCollaboratorSampleId", TEXT_KEYWORD_MAPPING,
             "ddpParticipantId", TEXT_KEYWORD_MAPPING,
-            "ddpInstanceId", TEXT_KEYWORD_MAPPING,
-            "institutionId", TEXT_KEYWORD_MAPPING,
-            "ddpInstitutionId", TEXT_KEYWORD_MAPPING,
-            "type", TEXT_KEYWORD_MAPPING,
-            "participantId", TEXT_KEYWORD_MAPPING,
-            "medicalRecordId", TEXT_KEYWORD_MAPPING,
-            "name", TEXT_KEYWORD_MAPPING,
-            "contact", TEXT_KEYWORD_MAPPING );
+            "ddpLabel", TEXT_KEYWORD_MAPPING,
+            "dsmKitRequest", TEXT_KEYWORD_MAPPING,
+            "kitTypeId", TEXT_KEYWORD_MAPPING,
+            "externalOrderStatus", TEXT_KEYWORD_MAPPING,
+            "externalOrderNumber", TEXT_KEYWORD_MAPPING
+            );
 
     private static final Map<String, Object> kitRequestShippingMigrate2 = Map.of (
-            "phone", TEXT_KEYWORD_MAPPING,
-            "fax", TEXT_KEYWORD_MAPPING,
-            "faxSent", DATE_MAPPING, //DATE?
-            "faxSentBy", TEXT_KEYWORD_MAPPING,
-            "faxConfirmed", DATE_MAPPING, //DATE?
-            "faxSent2", DATE_MAPPING, //DATE?
-            "faxSent2By", TEXT_KEYWORD_MAPPING,
-            "faxConfirmed2", DATE_MAPPING, //DATE?
-            "faxSent3", DATE_MAPPING, //DATE?
-            "faxSent3By", TEXT_KEYWORD_MAPPING);
+            "externalOrderDate", TEXT_KEYWORD_MAPPING,
+            "externalResponse", TEXT_KEYWORD_MAPPING,
+            "uploadReason", TEXT_KEYWORD_MAPPING,
+            "noReturn", BOOLEAN_MAPPING,
+            "createdBy", TEXT_KEYWORD_MAPPING,
+            "dsmKitRequestId", TEXT_KEYWORD_MAPPING,
+            "dsmKitId", TEXT_KEYWORD_MAPPING,
+            "kitComplete", BOOLEAN_MAPPING,
+            "labelUrlTo", TEXT_KEYWORD_MAPPING,
+            "labelUrlReturn", TEXT_KEYWORD_MAPPING
+    );
 
-    private static final Map<String, Object> medicalRecordMapping3 = Map.of (
-            "faxConfirmed3", DATE_MAPPING, //DATE?
-            "mrReceived", DATE_MAPPING, //DATE?
-            "followUps", TEXT_KEYWORD_MAPPING,
-            "mrDocument", TEXT_KEYWORD_MAPPING,
-            "mrDocumentFileName", TEXT_KEYWORD_MAPPING,
-            "mrProblem", BOOLEAN_MAPPING,
-            "mrProblemText", TEXT_KEYWORD_MAPPING,
-            "unableObtain", BOOLEAN_MAPPING,
-            "unableObtainText", TEXT_KEYWORD_MAPPING,
-            "duplicate", BOOLEAN_MAPPING);
+    private static final Map<String, Object> kitRequestShippingMigrate3 = Map.of (
+            "trackingToId", TEXT_KEYWORD_MAPPING,
+            "trackingReturnId", TEXT_KEYWORD_MAPPING,
+            "easypostTrackingToUrl", TEXT_KEYWORD_MAPPING,
+            "easypostTrackingReturnUrl", TEXT_KEYWORD_MAPPING,
+            "easypostToId", TEXT_KEYWORD_MAPPING,
+            "easypostShipmentStatus", BOOLEAN_MAPPING,
+            "scanDate", TEXT_KEYWORD_MAPPING,
+            "labelDate", TEXT_KEYWORD_MAPPING,
+            "error", BOOLEAN_MAPPING,
+            "message", TEXT_KEYWORD_MAPPING);
 
-    private static final Map<String, Object> medicalRecordMapping4 = Map.of (
-            "followUpRequired", BOOLEAN_MAPPING,
-            "followUpRequiredText", TEXT_KEYWORD_MAPPING,
-            "international", BOOLEAN_MAPPING,
-            "crRequired", BOOLEAN_MAPPING,
-            "pathologyPresent", TEXT_KEYWORD_MAPPING,
-            "notes", TEXT_KEYWORD_MAPPING,
-            "additionalValuesJson", TEXT_KEYWORD_MAPPING,
-            "reviewMedicalRecord", TEXT_KEYWORD_MAPPING
+    private static final Map<String, Object> kitRequestShippingMigrate4 = Map.of (
+            "receiveDate", TEXT_KEYWORD_MAPPING,
+            "deactivatedDate", TEXT_KEYWORD_MAPPING,
+            "easypostAddressIdTo", TEXT_KEYWORD_MAPPING,
+            "deactivationReason", TEXT_KEYWORD_MAPPING,
+            "trackingId", TEXT_KEYWORD_MAPPING,
+            "kitLabel", TEXT_KEYWORD_MAPPING,
+            "express", BOOLEAN_MAPPING,
+            "testResult", TEXT_KEYWORD_MAPPING,
+            "needsApproval", BOOLEAN_MAPPING,
+            "authorization", TEXT_KEYWORD_MAPPING
+    );
+
+    private static final Map<String, Object> kitRequestShippingMigrate5 = Map.of (
+            "denialReason", TEXT_KEYWORD_MAPPING,
+            "authorizedBy", TEXT_KEYWORD_MAPPING,
+            "upsTrackingStatus", TEXT_KEYWORD_MAPPING,
+            "upsReturnStatus", TEXT_KEYWORD_MAPPING,
+            "CEOrder", BOOLEAN_MAPPING // ? what is this ?
+    );
+
+    private static final Map<String, Object> KitRequestShippingMerged = MapAdapter.of(
+            kitRequestShippingMigrate1,
+            kitRequestShippingMigrate2,
+            kitRequestShippingMigrate3,
+            kitRequestShippingMigrate4,
+            kitRequestShippingMigrate5
     );
 
 
