e3e74d8a999565408cbc62ee49988b363ff7decd
Nika <nghurtch@broadinstitute.org>
2021-11-04 12:02:49 +0400

    DDP-7117 | finish OncHistoryDetails

--- src/main/java/org/broadinstitute/dsm/model/elastic/migrationscript/OncHistoryDetailsMigrate.java
+++ src/main/java/org/broadinstitute/dsm/model/elastic/migrationscript/OncHistoryDetailsMigrate.java
@@ -23,44 +23,67 @@ public class OncHistoryDetailsMigrate {
     private static final Map<String, Object> oncHistoryDetails2 = Map.of (
             "histology", TEXT_KEYWORD_MAPPING,
             "accessionNumber", TEXT_KEYWORD_MAPPING,
-            "facility", TEXT_KEYWORD_MAPPING, //DATE?
+            "facility", TEXT_KEYWORD_MAPPING,
             "phone", TEXT_KEYWORD_MAPPING,
-            "fax", TEXT_KEYWORD_MAPPING, //DATE?
-            "notes", TEXT_KEYWORD_MAPPING, //DATE?
+            "fax", TEXT_KEYWORD_MAPPING,
+            "notes", TEXT_KEYWORD_MAPPING,
             "additionalValuesJson", TEXT_KEYWORD_MAPPING,
-            "faxSentBy", TEXT_KEYWORD_MAPPING, //DATE?
+            "faxSentBy", TEXT_KEYWORD_MAPPING,
             "faxConfirmed", DATE_MAPPING, //DATE?
             "faxSent2", DATE_MAPPING ); //DATE?
 
     private static final Map<String, Object> oncHistoryDetails3 = Map.of (
-            "faxSent2By", TEXT_KEYWORD_MAPPING, //DATE?
+            "faxSent2By", TEXT_KEYWORD_MAPPING,
             "faxConfirmed2", DATE_MAPPING, //DATE?
             "faxSent3", DATE_MAPPING,
             "faxSent3By", TEXT_KEYWORD_MAPPING,
             "faxConfirmed3", DATE_MAPPING,
             "tissueReceived", TEXT_KEYWORD_MAPPING,
-            "tissueProblemOption", TEXT_KEYWORD_MAPPING, //HERE
-            "unableObtain", BOOLEAN_MAPPING,
-            "unableObtainText", TEXT_KEYWORD_MAPPING,
-            "duplicate", BOOLEAN_MAPPING);
+            "tissueProblemOption", TEXT_KEYWORD_MAPPING,
+            "gender", TEXT_KEYWORD_MAPPING,
+            "destructionPolicy", TEXT_KEYWORD_MAPPING,
+            "unableObtainTissue", BOOLEAN_MAPPING);
 
     private static final Map<String, Object> oncHistoryDetails4 = Map.of (
-            "followUpRequired", BOOLEAN_MAPPING,
-            "followUpRequiredText", TEXT_KEYWORD_MAPPING,
-            "international", BOOLEAN_MAPPING,
-            "crRequired", BOOLEAN_MAPPING,
-            "pathologyPresent", TEXT_KEYWORD_MAPPING,
-            "notes", TEXT_KEYWORD_MAPPING,
-            "additionalValuesJson", TEXT_KEYWORD_MAPPING,
-            "reviewMedicalRecord", TEXT_KEYWORD_MAPPING
+            "tissueId", TEXT_KEYWORD_MAPPING,
+            "countReceived", TEXT_KEYWORD_MAPPING,
+            "tissueType", TEXT_KEYWORD_MAPPING,
+            "tissueSite", TEXT_KEYWORD_MAPPING,
+            "hE", TEXT_KEYWORD_MAPPING,
+            "pathologyReport", TEXT_KEYWORD_MAPPING,
+            "collaboratorSampleId", TEXT_KEYWORD_MAPPING,
+            "blockSent", DATE_MAPPING, // DATE?
+            "scrollsReceived", DATE_MAPPING, // DATE?
+            "skId", TEXT_KEYWORD_MAPPING
     );
 
-    private static final Map<String, Object> oncHistoryDetailsMerged = new HashMap<>();
+    private static final Map<String, Object> oncHistoryDetails5 = Map.of (
+            "smId", TEXT_KEYWORD_MAPPING,
+            "sentGp", DATE_MAPPING, // DATE?,
+            "firstSmId", TEXT_KEYWORD_MAPPING,
+            "additionalTissueValueJson", TEXT_KEYWORD_MAPPING,
+            "expectedReturn", DATE_MAPPING, // DATE?????????????
+            "returnDate", DATE_MAPPING, // DATE?
+            "returnFedexId", TEXT_KEYWORD_MAPPING,
+            "shlWorkNumber", TEXT_KEYWORD_MAPPING,
+            "tumorPercentage", TEXT_KEYWORD_MAPPING,
+            "tissueSequence", TEXT_KEYWORD_MAPPING
+    );
 
-    static {
-        oncHistoryDetailsMerged.putAll(oncHistoryDetails1);
-        oncHistoryDetailsMerged.putAll(oncHistoryDetails2);
-        oncHistoryDetailsMerged.putAll(oncHistoryDetails3);
-        oncHistoryDetailsMerged.putAll(oncHistoryDetails4);
-    }
+    private static final Map<String, Object> oncHistoryDetails6 = Map.of (
+            "scrollsCount", TEXT_KEYWORD_MAPPING,
+            "ussCount", TEXT_KEYWORD_MAPPING,
+            "hECount", TEXT_KEYWORD_MAPPING,
+            "blocksCount", TEXT_KEYWORD_MAPPING
+    );
+
+    private static final Map<String, Object> oncHistoryDetailsMerged = MapAdapter.of(
+            oncHistoryDetails1,
+            oncHistoryDetails2,
+            oncHistoryDetails3,
+            oncHistoryDetails4,
+            oncHistoryDetails5,
+            oncHistoryDetails6
+            );
+
 }
