d467e4b1057cb8a4467e20cca92837325a328237
gmakhara <gmakhara@broadinstitute.org>
2021-11-09 18:54:52 +0400

    DDP-7117 | start creating of mapper for dynamic fields

--- src/main/java/org/broadinstitute/dsm/model/elastic/migrationscript/ParticipantDataMigrate.java
+++ src/main/java/org/broadinstitute/dsm/model/elastic/migrationscript/ParticipantDataMigrate.java
@@ -1,9 +1,12 @@
 package org.broadinstitute.dsm.model.elastic.migrationscript;
 
 
+import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.broadinstitute.dsm.db.dao.ddp.participant.ParticipantDataDao;
 import org.broadinstitute.dsm.db.dto.ddp.participant.ParticipantDataDto;
 import org.broadinstitute.dsm.statics.ESObjectConstants;
@@ -19,6 +22,35 @@ public class ParticipantDataMigrate extends BaseMigrator {
 
     @Override
     protected Map<String, List<Object>> getDataByRealm() {
-        return null;
+
+        Map<String, List<ParticipantDataDto>> participantDataByRealm = participantDataDao.getParticipantDataByRealm(realm);
+        return (Map) participantDataByRealm;
+    }
+
+    @Override
+    public void export() {
+        //decorate here
+        Map<String, List<Object>> dataByRealm = getDataByRealm();
+        try {
+            Field dataField = ParticipantDataDto.class.getField("data");
+            dataField.setAccessible(true);
+            for (Map.Entry<String, List<Object>> entry: dataByRealm.entrySet()) {
+                String participantId = entry.getKey();
+                List<Object> datas = entry.getValue();
+                for (Object data: datas) {
+                    ParticipantDataDto pDataDto  = (ParticipantDataDto) data;
+                    String json = (String) dataField.get(pDataDto);
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    try {
+                        objectMapper.readValue(json, Map.class);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        super.export();
     }
 }
