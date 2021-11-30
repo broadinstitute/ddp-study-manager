1c828c3239aca11afd3fffe53faa3f5ef4f1e168
Nika <nghurtch@broadinstitute.org>
2021-11-30 12:18:49 +0400

    DDP-7121 | generate mapping for dynamic fields

--- src/main/java/org/broadinstitute/dsm/model/elastic/migration/DynamicFieldsMappingMigrator.java
+++ src/main/java/org/broadinstitute/dsm/model/elastic/migration/DynamicFieldsMappingMigrator.java
@@ -36,25 +36,39 @@ public class DynamicFieldsMappingMigrator implements Exportable {
         List<FieldSettingsDto> fieldSettingsByStudyName = fieldSettingsDao.getFieldSettingsByStudyName(study);
         Parser parser = new DynamicFieldsTypeParser();
         Map<String, Object> propertyMap = new HashMap<>();
-        Map<String, Object> additionalValuesJson = new HashMap<>(Map.of("additionalValuesJson", new HashMap<>()));
 //        Map.of(propertyName, new HashMap<>(Map.of(PROPERTIES, "")))
         for (FieldSettingsDto fieldSettingsDto : fieldSettingsByStudyName) {
             String fieldType = fieldSettingsDto.getFieldType();
             // oD, m,t
             BaseGenerator.PropertyInfo propertyInfo = Util.TABLE_ALIAS_MAPPINGS.get(fieldType);
             if (propertyInfo != null) {
+                String columnName = Util.underscoresToCamelCase(fieldSettingsDto.getColumnName());
                 String propertyName = propertyInfo.getPropertyName();
                 Object typeMap = parser.parse(fieldSettingsDto.getDisplayType());
-                Map<String, Object> medicalRecord = (Map<String, Object>) propertyMap.get(propertyName);
-                medicalRecord
-                propertyMap.merge(propertyName, typeMap, (prev, curr) -> {
-                    Map<String, Object> valuesJson = (Map<String, Object>) ((Map) prev).get("additionalValuesJson");
-                    valuesJson.putAll((Map)curr);
-                    return prev;
-                });
+                if (!(propertyMap.containsKey(propertyName))) {
+                    Map<String, Object> additionalValuesJson = new HashMap<>(Map.of("additionalValuesJson", new HashMap<>(Map.of(PROPERTIES, new HashMap<>(Map.of(columnName, typeMap))))));
+                    propertyMap.put(propertyName, additionalValuesJson);
+                } else {
+                    Map<String, Object> map = (Map<String, Object>) propertyMap.get(propertyName);
+                    Map<String, Object> json = (Map<String, Object>) map.get("additionalValuesJson");
+                    Map<String, Object> properties = (Map<String, Object>) json.get(PROPERTIES);
+                    properties.putIfAbsent(columnName, typeMap);
+                }
+            } else {
+                String columnName = Util.underscoresToCamelCase(fieldSettingsDto.getColumnName());
+                String propertyName = "participantData";
+                Object typeMap = parser.parse(fieldSettingsDto.getDisplayType());
+                if (!(propertyMap.containsKey(propertyName))) {
+                    Map<String, Object> additionalValuesJson = new HashMap<>(Map.of("data", new HashMap<>()));
+                    additionalValuesJson.put(PROPERTIES, new HashMap<>(Map.of(columnName, typeMap)));
+                    propertyMap.put(propertyName, additionalValuesJson);
+                } else {
+                    Map<String, Object> map = (Map<String, Object>) propertyMap.get(propertyName);
+                    Map<String, Object> json = (Map<String, Object>) map.get("data");
+                    Map<String, Object> properties = (Map<String, Object>) json.get(PROPERTIES);
+                    properties.putIfAbsent(columnName, typeMap);
+                }
             }
-
-
         }
         Map<String, Object> dsmLevelProperties = new HashMap<>(Map.of(PROPERTIES, propertyMap));
         Map<String, Map<String, Object>> dsmLevel = new HashMap<>(Map.of(DSM_OBJECT, dsmLevelProperties));
---
+++ src/test/java/org/broadinstitute/dsm/model/elastic/migration/DynamicFieldsMappingMigratorTest.java
@@ -0,0 +1,21 @@
+package org.broadinstitute.dsm.model.elastic.migration;
+
+import junit.framework.TestCase;
+import org.broadinstitute.dsm.TestHelper;
+import org.broadinstitute.dsm.model.elastic.export.Exportable;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DynamicFieldsMappingMigratorTest {
+
+    @BeforeClass
+    public static void setUp() {
+        TestHelper.setupDB();
+    }
+
+    @Test
+    public void testExport() {
+        Exportable exportable = new DynamicFieldsMappingMigrator("", "angio");
+        exportable.export();
+    }
+}
