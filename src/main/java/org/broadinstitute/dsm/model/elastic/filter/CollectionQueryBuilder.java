46b00febc1530f3a06fe8c1a7c1a2dd169f95279
gmakhara <gmakhara@broadinstitute.org>
2021-12-15 16:27:41 +0400

    DDP-7121 | start making parser

--- src/main/java/org/broadinstitute/dsm/model/elastic/filter/CollectionQueryBuilder.java
+++ src/main/java/org/broadinstitute/dsm/model/elastic/filter/CollectionQueryBuilder.java
@@ -1,5 +1,11 @@
 package org.broadinstitute.dsm.model.elastic.filter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 import org.elasticsearch.index.query.AbstractQueryBuilder;
 
 public class CollectionQueryBuilder extends DsmAbstractQueryBuilder {
@@ -10,9 +16,27 @@ public class CollectionQueryBuilder extends DsmAbstractQueryBuilder {
 
     @Override
     public AbstractQueryBuilder build() {
-
+        Map<String, List<String>> filterByLogicalOperators = new HashMap<>();
         String filter = "AND m.medicalRecordId = 15 AND m.dynamicFields.ragac = 55 OR m.medicalRecordName = 213";
+        String[] andSeparated = filter.split("AND");
+        for (String eachFilter : andSeparated) {
+            String cleanedEachFilter = eachFilter.trim();
+            if (cleanedEachFilter.contains("OR")) {
 
+            } else {
+                filterByLogicalOperators.compute("AND", (curr, prev) -> {
+                    if (Objects.isNull(prev)) {
+                        List<String> filters = new ArrayList<>();
+                        filters.add(cleanedEachFilter);
+                        return filters;
+                    } else {
+                        prev.add(cleanedEachFilter);
+                        return prev;
+                    }
+                });
+
+            }
+        }
 
         return null;
     }
