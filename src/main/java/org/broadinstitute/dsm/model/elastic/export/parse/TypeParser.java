63bf035994669f35277b689f258d700d016332b6
gmakhara <gmakhara@broadinstitute.org>
2022-01-04 17:41:05 +0400

    DDP-7121 | start overriding of method parse of type parser

--- src/main/java/org/broadinstitute/dsm/model/elastic/export/parse/TypeParser.java
+++ src/main/java/org/broadinstitute/dsm/model/elastic/export/parse/TypeParser.java
@@ -1,5 +1,6 @@
 package org.broadinstitute.dsm.model.elastic.export.parse;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,6 +23,25 @@ public class TypeParser extends BaseParser {
     public static final Map<String, String> DATE_MAPPING = new HashMap<>(Map.of(MappingGenerator.TYPE, DATE));
     public static final String LONG = "long";
     public static final Map<String, String> LONG_MAPPING = new HashMap<>(Map.of(MappingGenerator.TYPE, LONG));
+
+    @Override
+    public Object parse(String fieldName) {
+        Class<?> propertyClass = propertyInfo.getPropertyClass();
+        Object mappingType;
+        try {
+            Field field = propertyClass.getField(fieldName);
+            if (long.class.isAssignableFrom(field.getType())) {
+                mappingType = LONG_MAPPING;
+            } else if (boolean.class.isAssignableFrom(field.getType())) {
+                mappingType = BOOLEAN_MAPPING;
+            } else {
+                // either text or date in string
+            }
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+        return super.parse(fieldName);
+    }
 
     @Override
     protected Object forNumeric(String value) {
--- src/test/java/org/broadinstitute/dsm/model/elastic/export/parse/TypeParserTest.java
+++ src/test/java/org/broadinstitute/dsm/model/elastic/export/parse/TypeParserTest.java
@@ -3,8 +3,12 @@ package org.broadinstitute.dsm.model.elastic.export.parse;
 import static org.broadinstitute.dsm.model.elastic.export.parse.TypeParser.*;
 import static org.junit.Assert.*;
 
+import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.dsm.db.MedicalRecord;
+import org.broadinstitute.dsm.model.NameValue;
+import org.broadinstitute.dsm.model.elastic.export.ExportFacadePayload;
 import org.broadinstitute.dsm.model.elastic.export.generate.BaseGenerator;
+import org.broadinstitute.dsm.model.elastic.export.generate.GeneratorPayload;
 import org.broadinstitute.dsm.model.elastic.migration.DynamicFieldsTypeParser;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,7 +17,7 @@ public class TypeParserTest {
 
 
     @Test
-    public void parse() {
+    public void parseDynamicFields() {
 
         BaseGenerator.PropertyInfo propertyInfo = new BaseGenerator.PropertyInfo(MedicalRecord.class, true);
         propertyInfo.setFieldName("additionalValuesJson");
@@ -22,20 +26,30 @@ public class TypeParserTest {
         typeParser.setPropertyInfo(propertyInfo);
         typeParser.setFieldName("scooby");
         typeParser.setDisplayType("TEXT");
-        Object parsedObject = typeParser.parse();
+        Object parsedObject = typeParser.parse(StringUtils.EMPTY);
         Assert.assertEquals(TEXT_KEYWORD_MAPPING, parsedObject);
 
         typeParser.setDisplayType("NUMBER");
-        Object parsedObject1 = typeParser.parse();
+        Object parsedObject1 = typeParser.parse(StringUtils.EMPTY);
         Assert.assertEquals(LONG_MAPPING, parsedObject1);
 
         typeParser.setDisplayType("DATE");
-        Object parsedObject2 = typeParser.parse();
+        Object parsedObject2 = typeParser.parse(StringUtils.EMPTY);
         Assert.assertEquals(DATE_MAPPING, parsedObject2);
 
         typeParser.setDisplayType("CHECKBOX");
-        Object parsedObject3 = typeParser.parse();
+        Object parsedObject3 = typeParser.parse(StringUtils.EMPTY);
         Assert.assertEquals(BOOLEAN_MAPPING, parsedObject3);
+    }
+
+    @Test
+    public void parseNonDynamicFields() {
+        NameValue crRequired = new NameValue("crRequired", true);
+        GeneratorPayload generatorPayload = new GeneratorPayload(crRequired);
+        ExportFacadePayload exportFacadePayload = new ExportFacadePayload("", "", generatorPayload, "");
+        TypeParser typeParser = TypeParserFactory.of(exportFacadePayload);
+        Object booleanMapping = typeParser.parse("true");
+        assertEquals(BOOLEAN_MAPPING, booleanMapping);
     }
 
 }
