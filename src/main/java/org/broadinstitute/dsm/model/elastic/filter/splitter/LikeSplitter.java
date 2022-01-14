c7d7292b892f5b2fd04f44cf9a024250b06e05a9
Nika <nghurtch@broadinstitute.org>
2022-01-14 18:16:02 +0400

    DDP-7121 | implement correct splitting mechanism for LikeSplitter

--- src/main/java/org/broadinstitute/dsm/model/elastic/filter/splitter/LikeSplitter.java
+++ src/main/java/org/broadinstitute/dsm/model/elastic/filter/splitter/LikeSplitter.java
@@ -3,10 +3,17 @@ package org.broadinstitute.dsm.model.elastic.filter.splitter;
 
 import org.broadinstitute.dsm.model.Filter;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class LikeSplitter extends BaseSplitter {
 
     @Override
     public String[] split() {
-        return filter.split(Filter.LIKE_TRIMMED);
+        String[] splittedFilter = filter.split(Filter.LIKE_TRIMMED);
+        return Arrays.stream(splittedFilter)
+                .map(str ->  str.replace("%",""))
+                .collect(Collectors.toList())
+                .toArray(new String[] {});
     }
 }
