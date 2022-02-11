adc2351172b4542036858858c8e0d8ff7934288f
Nika <nghurtch@broadinstitute.org>
2022-02-11 11:15:41 +0400

    DDP-7443 | fix gedFieldWithAlias()

--- src/main/java/org/broadinstitute/dsm/model/elastic/filter/splitter/JsonContainsSplitter.java
+++ src/main/java/org/broadinstitute/dsm/model/elastic/filter/splitter/JsonContainsSplitter.java
@@ -7,34 +7,37 @@ import org.broadinstitute.dsm.statics.DBConstants;
 import org.broadinstitute.dsm.statics.ESObjectConstants;
 import org.broadinstitute.dsm.util.ElasticSearchUtil;
 
+import java.util.Arrays;
+
 public class JsonContainsSplitter extends BaseSplitter {
+
+    public static final int ALIAS_INDEX = 0;
+    public static final int FIELD_INDEX = 1;
 
     @Override
     public String[] split() {
-        return filter.split(Filter.JSON_CONTAINS)[1]
+        return filter.split(Filter.JSON_CONTAINS)[FIELD_INDEX]
                 .replace(Filter.OPEN_PARENTHESIS, StringUtils.EMPTY)
-                .replace(Filter.CLOSE_PARENTHESIS,StringUtils.EMPTY)
+                .replace(Filter.CLOSE_PARENTHESIS, StringUtils.EMPTY)
                 .split(Filter.JSON_OBJECT);
 
     }
 
     @Override
     public String[] getValue() {
-        return new String[] {splittedFilter[1].split(Util.COMMA_SEPARATOR)[1].trim()};
-    }
-
-    @Override
-    public String getInnerProperty() {
-        return splittedFilter[1]
-                .split(Util.COMMA_SEPARATOR)[0]
-                .replaceAll(Filter.SINGLE_QUOTE, StringUtils.EMPTY);
+        return new String[]{splittedFilter[FIELD_INDEX].split(Util.COMMA_SEPARATOR)[FIELD_INDEX].trim()};
     }
 
     @Override
     protected String[] getFieldWithAlias() {
-        return splittedFilter[0]
+        String[] fieldWithAlias = splittedFilter[ALIAS_INDEX]
                 .replace(Util.COMMA_SEPARATOR, StringUtils.EMPTY)
                 .trim()
                 .split(ElasticSearchUtil.ESCAPE_CHARACTER_DOT_SEPARATOR);
+        String innerProperty = splittedFilter[FIELD_INDEX]
+                .replace(Filter.SINGLE_QUOTE, StringUtils.EMPTY)
+                .trim()
+                .split(Util.COMMA_SEPARATOR)[ALIAS_INDEX].trim();
+        return new String[]{fieldWithAlias[ALIAS_INDEX], fieldWithAlias[FIELD_INDEX], innerProperty};
     }
 }
