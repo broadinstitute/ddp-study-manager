e1a1bd00425afbcb3b5496865f85f8978a2423c3
pegahtah <ptaheri@broadinstitute.org>
2022-02-23 10:08:02 -0500

    changed the timing of query to 18 months (540 days) commented out the deployment of the other CF

--- build-and-deploy-cloud-functions.sh
+++ build-and-deploy-cloud-functions.sh
@@ -19,16 +19,16 @@ gcloud --project=${PROJECT_ID} functions deploy \
     --vpc-connector=projects/${PROJECT_ID}/locations/us-central1/connectors/appengine-default-connect
 
 
-echo "Deploying kit boston tracking to ${PROJECT_ID}"
-gcloud --project=${PROJECT_ID} functions deploy \
-    tbos-kit-ups-tracker \
-    --entry-point=org.broadinstitute.dsm.jobs.TestBostonUPSTrackingJob \
-    --runtime=java11 \
-    --trigger-topic=cf-kit-tracking \
-    --timeout=540\
-    --source=target/deployment \
-    --set-env-vars="PROJECT_ID=${PROJECT_ID},SECRET_ID=cf-kit-tracker,STUDY_MANAGER_SCHEMA=${STUDY_MANAGER_SCHEMA},STUDY_SERVER_SCHEMA=${STUDY_SERVER_SCHEMA}" \
-    --vpc-connector=projects/${PROJECT_ID}/locations/us-central1/connectors/appengine-default-connect
+#echo "Deploying kit boston tracking to ${PROJECT_ID}"
+#gcloud --project=${PROJECT_ID} functions deploy \
+#    tbos-kit-ups-tracker \
+#    --entry-point=org.broadinstitute.dsm.jobs.TestBostonUPSTrackingJob \
+#    --runtime=java11 \
+#    --trigger-topic=cf-kit-tracking \
+#    --timeout=540\
+#    --source=target/deployment \
+#    --set-env-vars="PROJECT_ID=${PROJECT_ID},SECRET_ID=cf-kit-tracker,STUDY_MANAGER_SCHEMA=${STUDY_MANAGER_SCHEMA},STUDY_SERVER_SCHEMA=${STUDY_SERVER_SCHEMA}" \
+#    --vpc-connector=projects/${PROJECT_ID}/locations/us-central1/connectors/appengine-default-connect
 
 
 
